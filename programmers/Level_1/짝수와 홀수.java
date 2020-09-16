f17388d9f4e215e54ba829649bf96b395c1e45ef
Jung Hyun Eom <djawjdgus93@naver.com>
2020-09-16 10:09:25 +0900

    Create 짝수와 홀수.java

---
+++ programmers/Level_1/짝수와 홀수.java
@@ -0,0 +1,14 @@
+/*
+Programmers
+Level 1 - Java
+연습문제
+짝수와 홀수
+
+https://programmers.co.kr/learn/courses/30/lessons/12937
+*/
+
+class Solution {
+    public String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+}
