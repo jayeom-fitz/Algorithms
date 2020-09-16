5db4ea80f484463fd43394d577973ce2e4807ffa
Jung Hyun Eom <djawjdgus93@naver.com>
2020-09-16 10:46:37 +0900

    Update 소수 찾기.java

--- programmers/Level_1/소수 찾기.java
+++ programmers/Level_1/소수 찾기.java
@@ -6,3 +6,20 @@ Level 1 - Java
 
 https://programmers.co.kr/learn/courses/30/lessons/12921
 */
+
+class Solution {
+  public int solution(int n) {
+      int answer = 0;
+      boolean[] prime = new boolean[n+1];
+      
+      for(int i=2; i<=n; i++){
+          if(!prime[i]){
+              answer++;
+              for(int j=i*2; j<=n; j+=i)
+                  prime[j] = true;
+          }
+      }
+      
+      return answer;
+  }
+}
