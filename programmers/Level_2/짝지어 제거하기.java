1d918c2a7fd0b1b7d4f37fc58170d899f8a139e5
Jung Hyun Eom <djawjdgus93@naver.com>
2020-09-21 13:57:38 +0900

    Create 짝지어 제거하기.java

---
+++ programmers/Level_2/짝지어 제거하기.java
@@ -0,0 +1,33 @@
+/*
+Programmers
+Level 2 - Java
+연습문제
+2017 팁스타운
+짝지어 제거하기
+
+https://programmers.co.kr/learn/courses/30/lessons/12973
+*/
+
+import java.util.Stack;
+
+class Solution
+{
+    public static int solution(String s)
+    {
+		Stack<Character> stack = new Stack<Character>();
+		int length = s.length();
+		
+		for(int i=0; i<length; i++) {
+			if(stack.isEmpty())
+				stack.push(s.charAt(i));
+			else {
+				if(stack.peek().equals(s.charAt(i)))
+					stack.pop();
+				else
+					stack.push(s.charAt(i));
+			}
+		}       
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
